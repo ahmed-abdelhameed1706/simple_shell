61e5243583801015795db96328aad252c23cf630
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-10 04:14:44 -0700

    adding README.md and shell.h

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* #Includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/*Prototypes*/
+
+#endif /* SHELL_H */
