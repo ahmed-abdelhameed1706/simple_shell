428e7006d39054e674bc83e933b70a6511cc6371
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-12 12:38:10 +0000

    Update main function
    added if to check for vaild commands only
    added function to get the command path

--- main.c
+++ main.c
@@ -14,7 +14,9 @@ int main(void)
 	{
 		buf = get_user_input();
 		argv = get_tokens(buf, deli);
-		execute(argv);
+		argv[0] = get_path(argv[0]);
+		if (argv[0])
+			execute(argv);
 
 		free_tokens(argv);
 	}
