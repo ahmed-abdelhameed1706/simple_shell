3f2fa0dd5df3d4d2b1ef7a8495932ba8c95f9271
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-15 10:19:32 +0000

    Update main function file
    add loop to check each command given by script_exe function

--- main.c
+++ main.c
@@ -7,31 +7,38 @@
  */
 int main(void)
 {
-	char *buf = NULL, *command, **argv, *deli = " \n";
-	int parse_return_value;
+	char *buf = NULL, *command, **argv, **commands, *deli = " \n";
+	int parse_return_value, i;
 
 	signal(SIGINT, SIG_IGN);
 	while (1)
 	{
 		buf = get_user_input();
-		argv = get_tokens(buf, deli);
-
-		parse_return_value = parse_input(argv, buf);
-		if (parse_return_value == 0)
+		commands = script_exe(buf);
+		
+		if (!commands)
 			continue;
 
-		command = get_path(argv[0]);
-
-		if (strcmp(command, "null") != 0)
+		for (i = 0; commands[i]; i++)
 		{
-			execute(command, argv);
-		}
-		else
-			perror("Error");
+			argv = get_tokens(commands[i], deli);
 
-		free_tokens(argv);
-		free(command);
+			parse_return_value = parse_input(argv, commands[i]);
+			if (parse_return_value == 0)
+				continue;
+
+			command = get_path(argv[0]);
+
+			if (strcmp(command, "null") != 0)
+				execute(command, argv);
+			else
+				perror("Error");
+
+			free_tokens(argv);
+			free(command);
+		}
 		free(buf);
+		free_tokens(commands);
 	}
 	return (0);
 }
