3cca78bb76456fe78ac4aa04471d68d1d76f1467
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-15 12:30:11 +0000

    Update script exe file
    added get_commands function to clean the code
    fix some memeory free errors

--- script_exe.c
+++ script_exe.c
@@ -4,17 +4,65 @@
 /**
  * script_exe - function to read a script file
  * @buffer: user input
+ * @re_buf: readed buffer from a file
+ * @commands: commands array
  * Return: pointer to pointer array of commands
 */
-char **script_exe(char *buffer)
+char **script_exe(char *buffer, char *re_buf, char **commands)
 {
+	int i, j = 0;
+	char tmp_buf[1];
 	ssize_t op, re;
 	size_t commands_size = 2, buf_size = 2;
-	char *re_buf = NULL, **commands = NULL, tmp_buf[1], *rm_new;
-	int i = 0, j = 0;
 
-	re_buf = malloc(sizeof(char) * buf_size);
-	commands = malloc(sizeof(char *) * commands_size);
+	op = open(buffer, O_RDONLY);
+	re = read(op, tmp_buf, 1);
+	if (op == -1 || re == -1)
+	{
+		free(re_buf);
+		free(commands);
+		perror("Error");
+		return (NULL);
+	}
+	for (i = 0; re; i++)
+	{
+		if (*tmp_buf == '\n')
+		{
+			re_buf[i] = *tmp_buf;
+			re_buf[i + 1] = '\0';
+			commands[j] = strdup(re_buf);
+
+			i = 0;
+			buf_size = 2;
+			commands_size++;
+			j++;
+
+			commands = realloc(commands, sizeof(char *) * commands_size);
+			re_buf = realloc(re_buf, sizeof(char) * buf_size);
+			re = read(op, tmp_buf, 1);
+			continue;
+		}
+		re_buf[i] = *tmp_buf;
+		buf_size++;
+		re_buf = realloc(re_buf, sizeof(char) * buf_size);
+		re = read(op, tmp_buf, 1);
+	}
+	commands[j] = NULL;
+	free(re_buf);
+	return (commands);
+}
+
+/**
+ * get_commands - get all commands from buffer
+ * @buffer: user input
+ * Return: pointer to pointer array of commands
+*/
+char **get_commands(char *buffer)
+{
+	char *re_buf = NULL, **commands = NULL, *rm_new;
+
+	re_buf = malloc(sizeof(char) * 2);
+	commands = malloc(sizeof(char *) * 2);
 	if (!commands || !re_buf)
 	{
 		free_tokens(commands);
@@ -33,45 +81,6 @@ char **script_exe(char *buffer)
 	buffer = &(buffer[2]);
 	rm_new = strchr(buffer, '\n');
 	*rm_new = '\0';
-	op = open(buffer, O_RDONLY);
-	re = read(op, tmp_buf, 1);
 
-	if (op == -1 || re == -1)
-	{
-		free(re_buf);
-		free_tokens(commands);
-		perror("Err");
-		return (NULL);
-	}
-
-	while (re != 0)
-	{
-		if (*tmp_buf == '\n')
-		{
-			re_buf[i] = *tmp_buf;
-			re_buf[i + 1] = '\0';
-			commands[j] = strdup(re_buf);
-
-			i = 0;
-			buf_size = 2;
-			commands_size++;
-			j++;
-
-			commands = realloc(commands, sizeof(char *) * commands_size);
-			re_buf = realloc(re_buf, sizeof(char) * buf_size);
-
-			re = read(op, tmp_buf, 1);
-			continue;
-		}
-		re_buf[i] = *tmp_buf;
-
-		buf_size++;
-
-		re_buf = realloc(re_buf, sizeof(char) * buf_size);
-		re = read(op, tmp_buf, 1);
-		i++;
-	}
-	commands[j] = NULL;
-	free(re_buf);
-	return (commands);
+	return (script_exe(buffer, re_buf, commands));
 }
