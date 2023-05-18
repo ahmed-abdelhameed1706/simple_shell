dc95e029e7d65198cfae8f1e20f54ccf7312ed79
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-18 20:05:23 +0000

    Add _getenv function

---
+++ _getenv.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+
+/**
+ * _getenv - get an environment variable
+ * @name: name of environment variable
+ * Return: environment variable value
+*/
+char *_getenv(const char *name)
+{
+	char **env = environ, *tmp;
+	char *token;
+
+	while (*env)
+	{
+		tmp = strdup(*env);
+		token = _strtok(tmp, "=");
+		if (strcmp(token, name) == 0)
+		{
+			token = _strtok(NULL, "=");
+			free(tmp);
+			return (token);
+		}
+		free(tmp);
+		env++;
+	}
+	return (NULL);
+}
