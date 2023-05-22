ab3858dc08b32fb4ac2dc8e8f6f08a370d1bb2be
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-05-22 17:49:22 +0000

    Update get_path function
    remove the static var path and use dynamic var instead
    split the function into 2 functions for readability and betty
    added free for the path variable after finsh

--- get_path.c
+++ get_path.c
@@ -1,35 +1,28 @@
 #include "shell.h"
 
 /**
- * get_path - get the command path
- * @command: enterd command
- * Return: comamnd path
+ * check_env_path - get the command path from
+ * PATH environment variable
+ * @command: enterd command from user
+ * Return: NULL when failed else command path
 */
-char *get_path(char *command)
+char *check_env_path(char *command)
 {
-	char *command_path, *token;
+	char *path, *command_path, *token;
 	int command_len, path_len, i;
-	char path[] = "/home/vagrant/bin:/home/vagrant/.local/bin:/usr/local/sbin:\
-			/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:\
-			/usr/games:/usr/local/games:/snap/bin";
 
-
-	if (command[0] == '/' && access(command, F_OK) == 0)
-	{
-		command = strdup(command);
-		return (command);
-	}
-	else if (command[0] == '/' && access(command, F_OK) == -1)
-	{
-		command = strdup("null");
-		return (command);
-	}
+	path = (_getenv("PATH"));
 	token = _strtok(path, ":");
+
 	while (token)
 	{
 		command_len = strlen(command);
 		path_len = strlen(token);
+
 		command_path = malloc(sizeof(char) * (command_len + path_len + 2));
+		if (!command_path)
+			return (NULL);
+
 		for (i = 0; token[i]; i++)
 			command_path[i] = token[i];
 
@@ -40,11 +33,44 @@ char *get_path(char *command)
 		command_path[path_len] = '\0';
 
 		if (access(command_path, F_OK) == 0)
+		{
+			free(path);
 			return (command_path);
+		}
 
 		free(command_path);
 		token = _strtok(NULL, ":");
 	}
+
+	return (NULL);
+}
+
+/**
+ * get_path - get the command path
+ * @command: enterd command from user
+ * Return: comamnd path
+*/
+char *get_path(char *command)
+{
+	char *command_path;
+
+	if (command[0] == '/' && access(command, F_OK) == 0)
+	{
+		command = strdup(command);
+		return (command);
+	}
+
+	else if (command[0] == '/' && access(command, F_OK) == -1)
+	{
+		command = strdup("null");
+		return (command);
+	}
+
+	command_path = check_env_path(command);
+
+	if (command_path)
+		return (command_path);
+
 	command = strdup("null");
 	return (command);
 }
