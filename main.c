553410a7fb379d3054b28c6da93907aff20ae861
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-05-24 01:06:40 -0700

    fixing

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ int main(int argc, char *argvv[])
 {
 	char *buf = NULL, *command, **argv, **commands = NULL, *deli = " \n";
 	char *delim = ";";
-	int parse_return_value, i, exit_status = EXIT_SUCCESS;
+	int parse_return_value, i;
 
 	if (exe_file(argc, argvv) == 0)
 		return (0);
 	while (1)
 	{
-		buf = get_user_input(exit_status);
+		buf = get_user_input();
 		if (strchr(buf, ';') != NULL)
 			commands = strip_tokens(buf, delim);
 		else
@@ -29,7 +29,6 @@ int main(int argc, char *argvv[])
 		}
 		for (i = 0; commands[i] != NULL; i++)
 		{
-			exit_status = EXIT_SUCCESS;
 			argv = get_tokens(commands[i], deli);
 			parse_return_value = parse_input(argv, commands[i], argvv[0]);
 			if (parse_return_value == 0)
@@ -40,7 +39,6 @@ int main(int argc, char *argvv[])
 			else
 			{
 				handle_errors(argv[0], argvv[0]);
-				exit_status = EXIT_FAILURE;
 			}
 			free_tokens(argv);
 			free(command);
